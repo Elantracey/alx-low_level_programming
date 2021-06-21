15b89ce0f6156b7afb144aad554cb65125455c39
Osatohanmwen Nosa-Ojo <elanfrances3@gmail.com>
2021-06-21 02:16:21 -0700

    variables

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -8,13 +8,13 @@
  */
 int main(void)
 {
-	char low;
-	/* print small letters alphabets */
-	for (low = 'a'; low <= 'z'; low++)
-        {
+       char low;
+       /* print small letters alphabets */
+       for (low = 'a'; low <= 'z'; low++)
+       {
 		putchar(low);
-	}
-	putchar('\n');
+       }
+       putchar('\n');
 
-	return (0);
+       return (0);
 }
