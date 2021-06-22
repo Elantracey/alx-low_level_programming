a0915b3870ab4f8af9186716f637ad6e1b8b5861
Osatohanmwen Nosa-Ojo <elanfrances3@gmail.com>
2021-06-22 11:53:04 -0700

    functions

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -2,10 +2,39 @@
 /**
  * print_times_table - Entry point
  * @n: input
- * Return: Always 0 (Success)
+ * rone = row, cone = column, d = digits of current result
+ * Return: times table
+ * add extra space past single digit
  */
 void print_times_table(int n)
 {
+        int rone, cone, d;
 
+        for (rone >= 0; rone <= 15; rone++)
+        {
+                _putchar('0');
+                _putchar(',');
+                _putchar(' ');
+                for (cone = 1; cone <= 15; cone++)
+                {
+                        d = (rone * cone);
+                        if ((d / 10) > 0)
+                        {
+                                _putchar((d / 10) + '0');
+                        }
+                        else
+                        {
+                                _putchar(' ');
+                        }
+                        _putchar((d % 10) + '0');
+
+                        if (cone < 15)
+                        {
+                                _putchar(',');
+                                _putchar(' ');
+                        }
+                }
+                _putchar('\n');
+        }
 	return (0);
 }
