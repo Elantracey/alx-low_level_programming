090d728c878006b576f5c2369f3e283f3b6ed79a
Elantracey <84736535+Elantracey@users.noreply.github.com>
2021-06-22 11:22:14 +0100

    Update 4-isalpha.c

--- 0x02-functions_nested_loops/4-isalpha.c
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -1,8 +1,7 @@
 #include "holberton.h"
-
 /**
  * _isalpha - a function that checks for a letter, lowercase or uppercase
- * 
+ * @c: single letter input
  * Return: 1 if int c is a letter, 0 if otherwise
  */
 int _isalpha(int c)
