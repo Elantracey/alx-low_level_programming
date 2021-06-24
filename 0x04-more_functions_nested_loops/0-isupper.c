62bc734f66bf1381003ff2e4978769bdc85f7f98
Osatohanmwen Nosa-Ojo <elanfrances3@gmail.com>
2021-06-24 10:38:38 -0700

    more functions

--- 0x04-more_functions_nested_loops/0-isupper.c
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -1,17 +1,18 @@
-#include "holberton.h"                                             
+#include "holberton.h"
+
 /**          
- * isupper - function that checks for upper case character
+ * _isupper - function that checks for upper case character
  * @c: input character
  * Return: 1, if uppercase, 0 if not
  */
 int _isupper(int c)
 {
-	if (c >= "A" && c <= "Z")
+	if (c >= 'A' && c <= 'Z')
 	{
-		Return: (1);
+		return (1);
 	}
 	else
 	{
-		Return: (0);
+		return (0);
 	}
 }
