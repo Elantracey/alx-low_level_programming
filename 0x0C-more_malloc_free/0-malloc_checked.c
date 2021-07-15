70dd0a8570eb5ffae707ba4002f7841174ee2051
Osatohanmwen Nosa-Ojo <elanfrances3@gmail.com>
2021-07-15 02:39:23 -0700

    more

---
+++ 0x0C-more_malloc_free/0-malloc_checked.c
@@ -0,0 +1,21 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include "holberton.h"
+
+/**
+ * malloc_checked - Entry point
+ *@b: string we need to duplicate
+ * Return: a pointer to the allocate memory
+ */
+void *malloc_checked(unsigned int b)
+{
+	void *p = NULL;
+
+	p = malloc(b);
+	if (p == NULL)
+	{
+		exit(98);
+	}
+	return (p);
+}
+
