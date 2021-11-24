f875637a99eb520e20c0c3181fcce273b3fc58be
Elantracey <84736535+Elantracey@users.noreply.github.com>
2021-11-25 00:58:02 +0100

    Update 5-island_perimeter.py
    changes

--- 0x1C-makefiles/5-island_perimeter.py
+++ 0x1C-makefiles/5-island_perimeter.py
@@ -3,38 +3,38 @@
 
 
 def island_perimeter(grid):
-        """returns the perimeter of island described in grid"""
-            p = 0
-                for row in range(len(grid)):
-                            for col in range(len(grid[0])):
-                                            if grid[row][col] == 1:
-                                                                if row - 1 < 0:
-                                                                                        p += 1
-                                                                                                        else:
-                                                                                                                                try:
-                                                                                                                                                            if grid[row - 1][col] == 0 or row - 1 < 0:
-                                                                                                                                                                                            p += 1
-                                                                                                                                                                                                                except:
-                                                                                                                                                                                                                                            pass
-                                                                                                                                                                                                                                                        if row + 1 > len(grid) - 1:
-                                                                                                                                                                                                                                                                                p += 1
-                                                                                                                                                                                                                                                                                                else:
-                                                                                                                                                                                                                                                                                                                        try:
-                                                                                                                                                                                                                                                                                                                                                    if grid[row + 1][col] == 0:
-                                                                                                                                                                                                                                                                                                                                                                                    p += 1
-                                                                                                                                                                                                                                                                                                                                                                                                        except:
-                                                                                                                                                                                                                                                                                                                                                                                                                                    pass
-                                                                                                                                                                                                                                                                                                                                                                                                                                                if col + 1 > len(grid[row]) - 1:
-                                                                                                                                                                                                                                                                                                                                                                                                                                                                        p += 1
-                                                                                                                                                                                                                                                                                                                                                                                                                                                                                        else:
-                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                try:
-                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                            if grid[row][col + 1] == 0:
-                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                            p += 1
-                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                except:
-                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                            pass
-                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                        try:
-                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                if grid[row][col - 1] == 0 or col - 1 < 0:
-                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                            p += 1
-                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                            except:
-                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                    pass
-                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                    return (p)
+    """returns the perimeter of island described in grid"""
+    p = 0
+    for row in range(len(grid)):
+        for col in range(len(grid[0])):
+            if grid[row][col] == 1:
+                if row - 1 < 0:
+                    p += 1
+                else:
+                    try:
+                        if grid[row - 1][col] == 0 or row - 1 < 0:
+                            p += 1
+                    except:
+                        pass
+                if row + 1 > len(grid) - 1:
+                    p += 1
+                else:
+                    try:
+                        if grid[row + 1][col] == 0:
+                            p += 1
+                    except:
+                        pass
+                if col + 1 > len(grid[row]) - 1:
+                    p += 1
+                else:
+                    try:
+                        if grid[row][col + 1] == 0:
+                            p += 1
+                    except:
+                        pass
+                try:
+                    if grid[row][col - 1] == 0 or col - 1 < 0:
+                        p += 1
+                except:
+                    pass
+    return (p)
