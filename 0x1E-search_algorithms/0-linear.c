0ed016649ff63a7fd89884d149dabae6c5568e49
Elantracey <84736535+Elantracey@users.noreply.github.com>
2022-05-11 01:51:44 -0700

    hope

--- 0x1E-search_algorithms/0-linear.c
+++ 0x1E-search_algorithms/0-linear.c
@@ -1,3 +1,5 @@
+#include <stdio.h>
+#include <stdlib.h>
 #include "search_algos.h"
 
 /**
@@ -9,7 +11,7 @@
  */
 int linear_search(int *array, size_t size, int value)
 {
-	unsigned int i;
+	size_t i = 0;
 	if (array == NULL)
 		return (-1);
 	for (i = 0; i < size; i++)
